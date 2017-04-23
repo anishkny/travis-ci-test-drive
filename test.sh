5ec6d3868ab8d4eb3a5ee1d675ca058901f37159
Anish Karandikar <anishkny@gmail.com>
2017-04-23 10:45:25 -0400

    Add docker

--- test.sh
+++ test.sh
@@ -1,6 +1,11 @@
 #!/bin/bash -x
 
-functions start
-functions deploy helloWorld --trigger-http
-functions call helloWorld
-functions stop
+# gcloud auth activate-service-account <your-service-account-email> --key-file /tmp/your-key.p12 --project <your-project-id>
+
+gcloud auth activate-service-account travis-ci-runner@realworld-firebase.iam.gserviceaccount.com --key-file ./gcpcred.json --project realworld-firebase
+gcloud info
+
+# functions start
+# functions deploy helloWorld --trigger-http
+# functions call helloWorld
+# functions stop
