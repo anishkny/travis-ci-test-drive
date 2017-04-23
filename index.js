24b906d467e44f370ae13e2f28e5e2f3c7dfbef0
Anish Karandikar <anishkny@gmail.com>
2017-04-23 01:22:48 -0400

    Cleaned up handler

--- index.js
+++ index.js
@@ -5,14 +5,5 @@
  * @param {Object} res Cloud Function response context.
  */
 exports.helloWorld = function helloWorld (req, res) {
-  if (req.body.message === undefined) {
-    // This is an error case, as "message" is required
-    res.status(400).send('No message defined!');
-  } else {
-    // Everything is ok
-    console.log(req.body.message);
-    res.status(200).send(JSON.stringify({
-      message: req.body.message,
-    }));
-  }
+  res.status(200).send(JSON.stringify({query: req.query}, null, 2) + "\n");
 };
