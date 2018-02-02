1d8ff39c427a7cb451a08027c65b5440f9bc2b9c
Anish Karandikar <anishkny@gmail.com>
2018-02-01 23:34:31 -0500

    Use --no-sandbox for puppeteer on linux
    See: https://github.com/GoogleChrome/puppeteer/blob/master/docs/troubleshooting.md#chrome-headless-fails-due-to-sandbox-issues

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 # travis-ci-test-drive
 
 [![Travis](https://img.shields.io/travis/anishkny/travis-ci-test-drive.svg)](https://travis-ci.org/anishkny/travis-ci-test-drive)
+
 A repo to play around with [Travis CI](https://travis-ci.org/anishkny/travis-ci-test-drive) integration for GitHub.
--- test/picsum.test.js
+++ test/picsum.test.js
@@ -1,7 +1,9 @@
 const puppeteer = require('puppeteer');
 
 it('should load random image', async () => {
-  const browser = await puppeteer.launch();
+  const browser = await puppeteer.launch({
+    args: ['--no-sandbox', '--disable-setuid-sandbox']
+  });
   const page = await browser.newPage();
   await page.goto('https://picsum.photos/200/300/?random');
   await page.screenshot({ path: 'random.png' });
